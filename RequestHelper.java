3f493bc46de4d17be19ce416847357a9700cc40b
Piotr Pucołowski <93589319+PiotrPucolowski@users.noreply.github.com>
2024-01-16 12:52:49 +0100

    Add files via upload
    Added classes to generate quote, translate quote to Polish

---
+++ RequestHelper.java
@@ -0,0 +1,23 @@
+package pl.honestit.demo.utils;
+
+import java.net.*;
+import java.net.http.*;
+
+public class RequestHelper {
+    
+    public static String getContent(String url) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse response = client.send(
+                                                HttpRequest.newBuilder()
+                                                    .uri(URI.create(url))
+                                                    .GET()
+                                                    .build(),
+                                                HttpResponse.BodyHandlers.ofString());  
+            return (String) response.body();
+        } catch (Exception ex) {
+            return "";
+        }
+    }
+    
+}
